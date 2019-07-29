5136e5355c5774556df8766b2c1a9a50340f8829
David <davidmh.mail@gmail.com>
2019-07-29 17:04:56 -0600

    Create a React app that consumes a Web Component created with Angular Elements

--- src/App.js
+++ src/App.js
@@ -1,26 +1,33 @@
-import React from 'react';
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+
+  componentDidMount() {
+    this.doGreetComponent.addEventListener('greeted', this.onGreetedEvent);
+  }
+
+  componentWillUnmount() {
+    this.doGreetComponent.removeEventListener('greeted', this.onGreetedEvent);
+  }
+
+  onGreetedEvent(event) {
+    alert(event.detail);
+  }
+
+  render() {
+    return (
+        <div className="App">
+          <header className="App-header">
+            <img src={logo} className="App-logo" alt="logo" />
+            <h1>React meets Angular</h1>
+            <p>That little component down at the bottom was created with Angular Elements, built as a custom Web Component and consumed by this React App!</p>
+          </header>
+          <do-greet name="David" ref={component => this.doGreetComponent = component} />
+        </div>
+    );
+  }
 }
 
 export default App;
